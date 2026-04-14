7311dcf78714206d4cd7c41c147b3de886ad8af9
Navin29-4-5 <navin11104@gmail.com>
2026-04-15 01:13:04 +0530

    deployment bug fix on order confirmation

--- app/(main)/order-confirmation/page.tsx
+++ app/(main)/order-confirmation/page.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from "react";
 import { Header } from "../_components/Header";
 import { Footer } from "../_components/Footer";
 import OrderConfirmationPage from "./OrderConfirmation";
+
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "Order Confirmed — Kavin Organics",
@@ -11,7 +14,11 @@ export default function OrderConfirmation() {
   return (
     <>
       <Header />
-      <OrderConfirmationPage />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <OrderConfirmationPage />
+      </Suspense>
+
       <Footer />
     </>
   );
